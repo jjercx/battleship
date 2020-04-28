11fc3796bdfdcb97fa4bccfc8a08ccc39d00f741
Juan José Ramírez <jjramirez@pucp.pe>
2020-04-27 20:19:44 -0500

    add basic resposive grid

--- src/game/GameScreen.js
+++ src/game/GameScreen.js
@@ -1,5 +1,38 @@
 import React from "react";
+import Tile from "game/Tile";
+import styled from "styled-components";
 
-const GameScreen = () => <p>GameScreen</p>;
+const Container = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Grid = styled.div`
+  flex: 1;
+  max-width: 800px;
+  max-height: 800px;
+`;
+
+const Row = styled.div`
+  height: 10%;
+  width: 100%;
+  display: flex;
+`;
+
+const GameScreen = ({ size = 10 }) => (
+  <Container>
+    <Grid>
+      {[...Array(size)].map((_, i) => (
+        <Row key={i.toString()}>
+          {[...Array(size)].map((_, j) => (
+            <Tile key={j.toString()} />
+          ))}
+        </Row>
+      ))}
+    </Grid>
+  </Container>
+);
 
 export default GameScreen;
--- src/game/Tile.js
+++ src/game/Tile.js
@@ -1,5 +1,20 @@
 import React from "react";
+import styled from "styled-components";
 
-const Tile = () => <p>Tile</p>;
+const Square = styled.div`
+  width: 10%;
+  height: 100%;
+  background-color: orangered;
+  margin: 2px;
+  position: relative;
+
+  &:after {
+    content: "";
+    float: left;
+    padding-top: 100%;
+  }
+`;
+
+const Tile = () => <Square />;
 
 export default Tile;
