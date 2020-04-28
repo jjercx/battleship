f2f269bd781c673f64298a54fc5fbe10a9791148
Juan José Ramírez <jjramirez@pucp.pe>
2020-04-27 22:17:55 -0500

    use css grid to add headers

--- src/game/GameScreen.js
+++ src/game/GameScreen.js
@@ -3,34 +3,46 @@ import Tile from "game/Tile";
 import styled from "styled-components";
 
 const Container = styled.div`
-  flex: 1;
+  width: 100vw;
+  height: 100vh;
   display: flex;
   align-items: center;
   justify-content: center;
 `;
 
-const Grid = styled.div`
-  flex: 1;
-  max-width: 800px;
-  max-height: 800px;
+const Letter = styled.span`
+  color: blue;
+  font-weight: bold;
+  place-self: center;
+  grid-column-start: ${(props) => props.col};
+  grid-row-start: ${(props) => props.row};
 `;
 
-const Row = styled.div`
-  height: 10%;
-  width: 100%;
-  display: flex;
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 4vmin repeat(10, 8vmin);
+  grid-template-rows: 4vmin repeat(10, 8vmin);
+  grid-gap: 2px;
 `;
+
+const A_CHAR_CODE = 65;
 
 const GameScreen = ({ size = 10 }) => (
   <Container>
     <Grid>
-      {[...Array(size)].map((_, i) => (
-        <Row key={i.toString()}>
-          {[...Array(size)].map((_, j) => (
-            <Tile key={j.toString()} />
-          ))}
-        </Row>
+      {[...Array(size)].map((_, col) => (
+        <Letter row={1} col={col + 2}>
+          {col + 1}
+        </Letter>
       ))}
+      {[...Array(size)].map((_, row) => (
+        <Letter col={1} row={row + 2}>
+          {String.fromCharCode(A_CHAR_CODE + row)}
+        </Letter>
+      ))}
+      {[...Array(size)].map((_, row) =>
+        [...Array(size)].map((_, col) => <Tile row={row + 2} col={col + 2} />)
+      )}
     </Grid>
   </Container>
 );
--- src/game/Tile.js
+++ src/game/Tile.js
@@ -1,20 +1,9 @@
-import React from "react";
 import styled from "styled-components";
 
-const Square = styled.div`
-  width: 10%;
-  height: 100%;
+const Tile = styled.div`
   background-color: orangered;
-  margin: 2px;
-  position: relative;
-
-  &:after {
-    content: "";
-    float: left;
-    padding-top: 100%;
-  }
+  grid-column-start: ${(props) => props.col};
+  grid-row-start: ${(props) => props.row};
 `;
-
-const Tile = () => <Square />;
 
 export default Tile;
