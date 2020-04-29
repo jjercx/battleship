14e8535144b5a1e16d06603d2ffc6be82e863a47
Juan José Ramírez <jjramirez@pucp.pe>
2020-04-29 14:22:41 -0500

    Updates

---
+++ precache-manifest.e4191e84f01894a1bd1cfc3934657d16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33ad0334d26518841b1fbb93b3642423",
+    "url": "/battleship/index.html"
+  },
+  {
+    "revision": "144988826c8ed0ff20d4",
+    "url": "/battleship/static/css/main.a1586cd1.chunk.css"
+  },
+  {
+    "revision": "95eb4e01417cdae529ec",
+    "url": "/battleship/static/js/2.e2c9717d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/battleship/static/js/2.e2c9717d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "144988826c8ed0ff20d4",
+    "url": "/battleship/static/js/main.76a7ea9f.chunk.js"
+  },
+  {
+    "revision": "f5c3d63460f3ed22dac2",
+    "url": "/battleship/static/js/runtime-main.da07c048.js"
+  },
+  {
+    "revision": "1e31cf881a3ea91cdb5c821eb1d63485",
+    "url": "/battleship/static/media/radargrid.1e31cf88.png"
+  }
+]);
