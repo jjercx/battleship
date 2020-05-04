54abf24d408502e83872bb0c1af7dd98f9992226
Juan José Ramírez <jjramirez@pucp.pe>
2020-05-03 20:58:29 -0500

    restore data

--- src/game/models/ships.data.js
+++ src/game/models/ships.data.js
@@ -2,23 +2,19 @@ export default {
   data: [
     {
       count: 1,
+      size: 4,
+    },
+    {
+      count: 2,
+      size: 3,
+    },
+    {
+      count: 3,
+      size: 2,
+    },
+    {
+      count: 4,
       size: 1,
     },
-    // {
-    //   count: 1,
-    //   size: 4,
-    // }
-    // {
-    //   count: 2,
-    //   size: 3,
-    // },
-    // {
-    //   count: 3,
-    //   size: 2,
-    // },
-    // {
-    //   count: 4,
-    //   size: 1,
-    // },
   ],
 };
